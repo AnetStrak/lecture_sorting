fec3a5e716cbb44ff17a956b628db7cfb466afbc
AnetStrak <257046@vut.cz>
2024-04-08 12:27:34 +0200

    1

--- searching.py
+++ searching.py
@@ -11,12 +11,25 @@ def read_data(file_name, field):
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
-    file_path = os.path.join(cwd_path, file_name)
+    #   file_path = os.path.join(cwd_path, file_name)
 
+    import json
+    with open("sequential.json", "r") as file_obj:
+        allowed_keys = json.load(file_obj) ["allowed_keys"]
+
+
+    if field not in allowed_keys:
+        return None
+
+    with open(file_name, "r") as file_obj:
+        data = json.load(file_obj)
+
+    return data.get(field)
 
 def main():
     pass
-
+    sequential_data = read_data ("sequential.json", "unordered_numbers")
+    print("Sequential Data:", sequential_data)
 
 if __name__ == '__main__':
     main()
